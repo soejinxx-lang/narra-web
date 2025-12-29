8a2e6741eeaaf36ff348543454809a1feb05962d
soejinxx-lang <soejinxx@gmail.com>
2025-12-29 18:41:09 +0900

    Update page.tsx

---
+++ app/browse/recommended/page.tsx
@@ -0,0 +1,54 @@
+// app/browse/recommended/page.tsx
+
+import { fetchNovels } from "@/lib/api";
+
+export const dynamic = "force-dynamic";
+
+export default async function RecommendedNovelPage() {
+  const novels = await fetchNovels();
+
+  if (!novels || novels.length === 0) {
+    return null;
+  }
+
+  const recommended = [...novels].sort((a, b) => {
+    const ta = Number(a.id.replace("novel-", ""));
+    const tb = Number(b.id.replace("novel-", ""));
+    return tb - ta;
+  })[0];
+
+  return (
+    <div
+      style={{
+        padding: "24px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <a
+        href={`/novels/${recommended.id}`}
+        style={{
+          textDecoration: "none",
+          color: "inherit",
+        }}
+      >
+        <div
+          style={{
+            width: "220px",
+            aspectRatio: "2 / 3",
+            border: "1px solid #e5e5e5",
+            borderRadius: "8px",
+            background: "#fff",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            padding: "12px",
+          }}
+        >
+          {recommended.title}
+        </div>
+      </a>
+    </div>
+  );
+}
