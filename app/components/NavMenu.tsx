4a865300f0ab2b505f4954a7f6c05d7254c8cd06
soejinxx-lang <soejinxx@gmail.com>
2025-12-29 18:36:02 +0900

    Update NavMenu.tsx

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -14,9 +14,9 @@ export default function NavMenu() {
     >
       <li><Link href="/novels">All Novels</Link></li>
       <li><Link href="/browse">Browse</Link></li>
-      <li><Link href="/about">About NARRA</Link></li>
-      <li><Link href="/notes">Translation Notes</Link></li>
-      <li><Link href="/contact">Contact</Link></li>
+      <li><Link href="/guide">Guide</Link></li>
+      <li><Link href="/notes">Notes</Link></li>
+      <li><Link href="/support">Support</Link></li>
     </ul>
   );
 }
