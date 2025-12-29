44f3e36227fc6502e88561609242e6f06a1453ce
soejinxx-lang <soejinxx@gmail.com>
2025-12-29 18:47:45 +0900

    Create page.tsx

---
+++ app/browse/page.tsx
@@ -0,0 +1,44 @@
+// app/browse/page.tsx
+
+import Link from "next/link";
+
+export default function BrowsePage() {
+  return (
+    <div
+      style={{
+        padding: "24px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+      }}
+    >
+      <Link
+        href="/browse/new"
+        style={{
+          textDecoration: "none",
+          color: "black",
+          border: "1px solid #e5e5e5",
+          padding: "16px",
+          borderRadius: "8px",
+          background: "#fff",
+        }}
+      >
+        New
+      </Link>
+
+      <Link
+        href="/browse/recommend"
+        style={{
+          textDecoration: "none",
+          color: "black",
+          border: "1px solid #e5e5e5",
+          padding: "16px",
+          borderRadius: "8px",
+          background: "#fff",
+        }}
+      >
+        Recommend
+      </Link>
+    </div>
+  );
+}
