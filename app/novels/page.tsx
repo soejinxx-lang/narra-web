191b586889d4898fbcb2634a2320a3b64535af61
soejinxx-lang <soejinxx@gmail.com>
2025-12-29 19:45:05 +0900

    Update page.tsx

--- app/novels/page.tsx
+++ app/novels/page.tsx
@@ -41,8 +41,22 @@ export default async function Page() {
                   background: "#e5e5e5",
                   borderRadius: 10,
                   margin: 10,
+                  overflow: "hidden",
                 }}
-              />
+              >
+                {novel.cover_url && (
+                  <img
+                    src={novel.cover_url}
+                    alt={novel.title}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                      display: "block",
+                    }}
+                  />
+                )}
+              </div>
 
               <div
                 style={{
