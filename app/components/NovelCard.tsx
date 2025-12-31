9cf5ba35968e881469c45a3d0110a359831bb69c
soejinxx-lang <soejinxx@gmail.com>
2025-12-31 23:33:27 +0900

    Update NovelCard.tsx

--- app/components/NovelCard.tsx
+++ app/components/NovelCard.tsx
@@ -11,36 +11,47 @@ export default function NovelCard({ novel }: NovelCardProps) {
         aspectRatio: "2 / 3",
         border: "1px solid #e5e5e5",
         borderRadius: 16,
-        background: "#fff",
+        background: "#fff", // ← 카드 배경은 계속 흰색
         display: "flex",
         flexDirection: "column",
         overflow: "hidden",
       }}
     >
+      {/* 이미지 영역 */}
       <div
         style={{
           flex: 1,
           margin: 12,
           borderRadius: 14,
-          padding: 4,
-          border: "1px solid #243A6E",
-          overflow: "hidden",
-          background: "#fff",
+          background: "#fff", // ← 여기도 흰색
+          padding: 4,          // ← 이 padding이 “사이 공간”
         }}
       >
-        {novel.cover_url && (
-          <img
-            src={novel.cover_url}
-            alt={novel.title}
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              display: "block",
-              borderRadius: 10,
-            }}
-          />
-        )}
+        {/* 남색 띠 전용 레이어 */}
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            background: "#243A6E", // ← 딱 이 레이어만 남색
+            padding: 3,
+            borderRadius: 12,
+          }}
+        >
+          {novel.cover_url && (
+            <img
+              src={novel.cover_url}
+              alt={novel.title}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                display: "block",
+                borderRadius: 9,
+                background: "#fff",
+              }}
+            />
+          )}
+        </div>
       </div>
 
       <div
