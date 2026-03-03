e2d763fd70c622ec03212b49e163bdd25ad9d801
soejinxx-lang <soejinxx@gmail.com>
2026-03-03 17:06:20 +0900

    xxx

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function LibraryRedirect() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/mypage");
-  }, [router]);
-  return null;
+  redirect("/mypage");
 }
